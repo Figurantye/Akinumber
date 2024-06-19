9d2aeb80bbd7a2a92258d052eea07d031d4bc955
Gustavo Wazlawick <eva01.gustavo@gmail.com>
2024-06-19 10:20:30 -0300

    criado código de adivinhação

---
+++ guessinGame.js
@@ -0,0 +1,21 @@
+const leitor = require("readline-sync")
+
+function guessNumber() {
+    console.log("Advinhe um número de 1 a 100!");
+    let option = 0;
+    let ans = Math.floor(Math.random() * (100 - 1) + 1);
+    for (let index = 1; option != ans; index++) {
+        option = leitor.questionInt(`Tentativa ${index}: `)
+        if (option > ans) {
+            console.log("Muito alto! \nInsira um numero menor!\n ");
+        } else if (option < ans) {
+            console.log("Muito Baixo! \nInsira um numero maior!\n ");   
+        } else if (option == ans) {
+            console.log("Resposta certa!");
+        }else {
+            console.log("Numero inválido!");
+        }
+    }
+}
+
+guessNumber()
